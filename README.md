df28ffed8792433cd3eb1149f2121b3532ade8a7
Dor-sketch <dor.pascal@mail.huji.ac.il>
2023-10-30 20:57:05 +0200

    Add README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,22 @@
+# SmartReviwPad - An Anki Add-on for effective reviewing
+
+## Introduction
+
+SmartReviewPad is an Anki add-on that helps you review your cards more effectively. It is based on the idea of [SuperMemo](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2) and [Anki Simulator](
+https://pheartheceal.github.io/anki-optimizer/). It is designed to be used with the [Anki Simulator](https://pheartheceal.github.io/anki-optimizer/) to optimize your Anki settings.
+
+## Features
+
+- **Interactive Front Side**: You can interact with the front side of your cards. This allows you to answer questions, fill in the blanks, etc. directly on the front side of your cards. This is especially useful for language learning.
+- **Automatic Back Side**: The back side of your cards is automatically generated based on the front side. This allows you to create cards more quickly.
+- **Automatic Review**: You can review your cards directly in the editor. This allows you to review your cards more effectively.
+
+## Installation
+
+1. Download the latest version of SmartReviewPad from [here](https://github.com/Dor-sketch/Anki-SmartReviewPad.git).
+2. Copy the `SmartReviewPad` folder to your Anki add-ons folder. You can find the add-ons folder by going to `Tools > Add-ons > View Files...` in Anki.
+3. Restart Anki.
+
+## Usage
+
+WIP - Will be updated soon.
--- __init__.py
+++ __init__.py
@@ -14,13 +14,6 @@ def my_function():
     # Build the HTML for the front and back of the card
     front_html = f"<div id='front-container' style='display: flex; flex-direction: column; align-items: center; font-size: 24px;'><div class='field'>{front_field}</div></div>"
 
-
-    # back_html = "<div id='back-container' style='display: none; text-align: center; font-size: 24px;'>"
-    # for field in fields:
-    #     if not field.startswith("[sound:") and field.strip():
-    #         back_html += f"<div class='field'>{field}</div>"
-    # back_html += "</div>"
-
     # Build the HTML for the back of the card
     back_html = "<div id='back-container' style='display: none; text-align: center; font-size: 24px;'>"
     for field in fields:
@@ -31,26 +24,6 @@ def my_function():
 
     # Build the HTML for the scratchpad
     scratchpad_html = "\n\n\n<div id='scratchpad-container'><textarea id='scratchpad' dir='rtl' style='font-size: 24px; background-color: rgb(245, 245, 245);'></textarea></div>"
-
-    # Add a margin to the field element and change its font family
-    style = """
-        #field {
-            margin-top: 10px;
-            margin-bottom: 10px;
-            font-family: "Times New Roman", serif;
-        }
-        #scratchpad {
-            height: 100px;
-            background-color: transparent;
-            border: none;
-            resize: none;
-            outline: none;
-            overflow: hidden;
-            font-family: "Times New Roman", serif;
-        }
-    }
-        }
-    """
 
     # Update the card HTML to show the front, scratchpad, and back with the back fields initially hidden
     mw.reviewer.web.eval(f"""
@@ -78,8 +51,6 @@ def my_function():
         });
     """)
 
-    # self.mx.reviewer.state == "question":
-    #     self._getTypedAnswer()
 
 addHook("showQuestion", my_function)
 
@@ -91,37 +62,3 @@ def focus_scratchpad():
     """)
 
 addHook("showQuestion", focus_scratchpad)
-
-
-
-
-
-
-# # function of the shorcut should be different depending on the state of the previewer
-# def shortcutFunction(self, response: int) -> None:
-#     if self.state == "question":
-#         self._getTypedAnswer()
-#     elif self.state == "answer":
-#         self._answerCard(response)
-
-
-# def setUpShortcuts(self, _old):
-#     cuts = _old(self)
-
-#     # change the function called by each shorcut
-#     for i, shortcut in enumerate(cuts):
-#         if shortcut[0] == "1":
-#             cuts[i] = ("1", lambda: self.shortcutFunction(1))
-#         elif shortcut[0] == "2":
-#             cuts[i] = ("2", lambda: self.shortcutFunction(2))
-#         elif shortcut[0] == "3":
-#             cuts[i] = ("3", lambda: self.shortcutFunction(3))
-#         elif shortcut[0] == "4":
-#             cuts[i] = ("4", lambda: self.shortcutFunction(4))
-
-#     return cuts
-
-
-# Reviewer.shortcutFunction = shortcutFunction
-# Reviewer._shortcutKeys = wrap(
-#     Reviewer._shortcutKeys, setUpShortcuts, "around")
