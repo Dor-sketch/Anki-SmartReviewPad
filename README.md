562658bad473c1a8f856d0964ac749c4863ba1ad
Dor-sketch <dor.pascal@mail.huji.ac.il>
2023-10-30 21:16:57 +0200

    Refactore to OOP, Add gitignore, Update README.md

--- README.md
+++ README.md
@@ -1,22 +1,42 @@
-# SmartReviwPad - An Anki Add-on for effective reviewing
+# SmartReviewPad - An Anki Add-on for Effective Reviewing 📚
 
-## Introduction
+## Introduction 🌟
 
-SmartReviewPad is an Anki add-on that helps you review your cards more effectively. It is based on the idea of [SuperMemo](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2) and [Anki Simulator](
-https://pheartheceal.github.io/anki-optimizer/). It is designed to be used with the [Anki Simulator](https://pheartheceal.github.io/anki-optimizer/) to optimize your Anki settings.
+SmartReviewPad is an Anki add-on that helps you review your cards more effectively. It's based on the proven methodologies of [SuperMemo](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2) and [Anki Simulator](https://pheartheceal.github.io/anki-optimizer/). Use it in conjunction with [Anki Simulator](https://pheartheceal.github.io/anki-optimizer/) to optimize your Anki settings.
 
-## Features
+## Features 🛠️
 
-- **Interactive Front Side**: You can interact with the front side of your cards. This allows you to answer questions, fill in the blanks, etc. directly on the front side of your cards. This is especially useful for language learning.
-- **Automatic Back Side**: The back side of your cards is automatically generated based on the front side. This allows you to create cards more quickly.
-- **Automatic Review**: You can review your cards directly in the editor. This allows you to review your cards more effectively.
+- **Interactive Front Side**: Interact directly with the front side of your cards. This feature supports answering questions, filling in the blanks, and more. It's especially useful for language learning. 🌍
 
-## Installation
+- **Automatic Back Side**: The back side of your cards is auto-generated based on your input on the front side, speeding up the card creation process. 🔄
 
-1. Download the latest version of SmartReviewPad from [here](https://github.com/Dor-sketch/Anki-SmartReviewPad.git).
-2. Copy the `SmartReviewPad` folder to your Anki add-ons folder. You can find the add-ons folder by going to `Tools > Add-ons > View Files...` in Anki.
-3. Restart Anki.
+- **Automatic Review**: Review your cards directly in the editor, thereby making your reviewing sessions more effective. 🚀
 
-## Usage
+## Installation 📥
 
-WIP - Will be updated soon.
+1️⃣ Download the latest version of SmartReviewPad from [here](https://github.com/Dor-sketch/Anki-SmartReviewPad.git).
+
+2️⃣ Copy the `SmartReviewPad` folder to your Anki add-ons folder. Navigate to `Tools > Add-ons > View Files...` in Anki to find the add-ons folder.
+
+3️⃣ Restart Anki to complete the installation.
+
+## Usage 📝
+
+1️⃣ Open Anki and start reviewing a deck.
+
+2️⃣ The add-on will automatically enhance your review session based on the SmartReviewPad features.
+
+3️⃣ For more advanced settings, navigate to `Tools > Add-ons > SmartReviewPad > Config`.
+
+## Code Overview 🖥️
+
+The add-on is implemented in Python, extending Anki's internal functionality. It uses several best practices for code readability and efficiency. For more information, see the [code comments](https://github.com/Dor-sketch/Anki-SmartReviewPad/blob/main/main.py).
+
+## Contributing 🤝
+
+If you find a bug or want to contribute to the code, feel free to open an issue or submit a pull request on GitHub.
+
+## License 📜
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
--- __init__.py
+++ __init__.py
@@ -3,62 +3,98 @@ from anki.hooks import addHook
 from aqt.reviewer import Reviewer
 from anki.hooks import wrap
 
-def my_function():
-    # Get the current card and its fields
-    card = mw.reviewer.card
-    fields = card.note().fields
 
-    # Find the first non-empty field
-    front_field = next((field for field in fields if not field.startswith("[sound:") and field.strip()), None)
+class CardReviewer:
+    """
+    CardReviewer is a class that manages the card layout during the review process.
+    """
 
-    # Build the HTML for the front and back of the card
-    front_html = f"<div id='front-container' style='display: flex; flex-direction: column; align-items: center; font-size: 24px;'><div class='field'>{front_field}</div></div>"
+    def __init__(self, reviewer):
+        self.reviewer = reviewer
 
-    # Build the HTML for the back of the card
-    back_html = "<div id='back-container' style='display: none; text-align: center; font-size: 24px;'>"
-    for field in fields:
-        if "sound" not in field and field.strip() and field not in front_field:
-            back_html += f"<div class='field'>{field}</div>"
-    back_html += "</div>"
+    def update_card_review(self):
+        """
+        Updates the card layout during the review process.
+        """
+        card = mw.reviewer.card
+        fields = card.note().fields
+        front_field = self.find_first_non_empty_field(fields)
 
+        front_html = self.build_front_html(front_field)
+        back_html = self.build_back_html(fields, front_field)
+        scratchpad_html = self.build_scratchpad_html()
 
-    # Build the HTML for the scratchpad
-    scratchpad_html = "\n\n\n<div id='scratchpad-container'><textarea id='scratchpad' dir='rtl' style='font-size: 24px; background-color: rgb(245, 245, 245);'></textarea></div>"
+        self.inject_card_html(front_html, scratchpad_html, back_html)
+        self.add_keydown_event_listener()
 
-    # Update the card HTML to show the front, scratchpad, and back with the back fields initially hidden
-    mw.reviewer.web.eval(f"""
-        var card = document.querySelector('.card');
-        card.innerHTML = `{front_html}<br><br>\n\n\n{scratchpad_html}\n<br>{back_html}`;
-        card.style.display = 'flex';
-        card.style.flexDirection = 'column';
-        card.style.alignItems = 'center';
-        card.style.justifyContent = 'space-between';
-        card.style.height = '100%';
-        card.style.overflow = 'hidden';
-    """)
+    def find_first_non_empty_field(self, fields):
+        """
+        Finds the first non-empty field in a list of fields.
+        """
+        return next((field for field in fields if not field.startswith("[sound:") and field.strip()), None)
 
-    # Add a keydown event listener to the scratchpad
-    mw.reviewer.web.eval("""
-        var scratchpad = document.querySelector('#scratchpad');
-        scratchpad.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
-                // Show the back of the card
-                document.querySelector('#back-container').style.display = 'block';
-                // Move the cursor out of the scratchpad
-                scratchpad.blur();
-                event.preventDefault();
-            }
-        });
-    """)
+    def build_front_html(self, front_field):
+        """
+        Builds the HTML for the front of the card.
+        """
+        return f"<div id='front-container' style='display: flex; flex-direction: column; align-items: center; font-size: 24px;'><div class='field'>{front_field}</div></div>"
 
+    def build_back_html(self, fields, front_field):
+        """
+        Builds the HTML for the back of the card.
+        """
+        back_html = "<div id='back-container' style='display: none; text-align: center; font-size: 24px;'>"
+        for field in fields:
+            if "sound" not in field and field.strip() and field not in front_field:
+                back_html += f"<div class='field'>{field}</div>"
+        back_html += "</div>"
+        return back_html
 
-addHook("showQuestion", my_function)
+    def build_scratchpad_html(self):
+        """
+        Builds the HTML for the scratchpad.
+        """
+        return "<div id='scratchpad-container'><textarea id='scratchpad' dir='rtl' style='font-size: 24px; background-color: rgb(245, 245, 245);'></textarea></div>"
 
+    def inject_card_html(self, front_html, scratchpad_html, back_html):
+        """
+        Injects the HTML into the card layout.
+        """
+        mw.reviewer.web.eval(f"""
+            var card = document.querySelector('.card');
+            card.innerHTML = `{front_html}<br><br>\n\n\n{scratchpad_html}\n<br>{back_html}`;
+            card.style.display = 'flex';
+            card.style.flexDirection = 'column';
+            card.style.alignItems = 'center';
+            card.style.justifyContent = 'space-between';
+            card.style.height = '100%';
+            card.style.overflow = 'hidden';
+        """)
 
-def focus_scratchpad():
-    mw.reviewer.web.eval("""
-        var scratchpad = document.querySelector('#scratchpad');
-        scratchpad.focus();
-    """)
+    def add_keydown_event_listener(self):
+        """
+        Adds a keydown event listener to the scratchpad.
+        """
+        mw.reviewer.web.eval("""
+            var scratchpad = document.querySelector('#scratchpad');
+            scratchpad.addEventListener('keydown', function(event) {
+                if (event.key === 'Enter') {
+                    document.querySelector('#back-container').style.display = 'block';
+                    scratchpad.blur();
+                    event.preventDefault();
+                }
+            });
+        """)
 
-addHook("showQuestion", focus_scratchpad)
+    def focus_scratchpad(self):
+        """
+        Focuses the cursor on the scratchpad.
+        """
+        mw.reviewer.web.eval("""
+            var scratchpad = document.querySelector('#scratchpad');
+            scratchpad.focus();
+        """)
+
+# Hook into Anki
+card_reviewer = CardReviewer(mw.reviewer)
+addHook("showQuestion", card_reviewer.update_card_review)
